aa53ed8470a4349942467f1b7c2a5fac7da3881a
Dulsara Dhanuka <dulsara.dhanuka@gmail.com>
2025-05-07 14:42:00 +0530

    Stopped screen timeout because the vm freezes and added packages that were used in i3

--- Andromeda/desktop/Nembus/i3/i3.nix
+++ Andromeda/desktop/Nembus/i3/i3.nix
@@ -2,11 +2,13 @@
 
 {
   environment.systemPackages = with pkgs; [
+    dex
+    xss-lock
+    nm-applet
     i3-resurrect
   ];
 
   services.xserver.enable = true;
-  services.xserver.xautolock.time = 9999999;
   services.xserver.windowManager.i3.enable = true;
   services.xserver.windowManager.i3.configFile = ./config;
 }
