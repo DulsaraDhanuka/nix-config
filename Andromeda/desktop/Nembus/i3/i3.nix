76d5cc3aa560bf929bfb43a1472b48bcbc2b00e7
Dulsara Dhanuka <dulsara.dhanuka@gmail.com>
2025-05-09 01:25:14 +0530

    Fixed the python issue and wallpaper issue. Added keybindings for switching to dual monitor mode and added some wallpapers

--- Andromeda/desktop/Nembus/i3/i3.nix
+++ Andromeda/desktop/Nembus/i3/i3.nix
@@ -6,9 +6,12 @@
     xss-lock
     networkmanagerapplet
     i3-resurrect
+    nitrogen
   ];
 
   services.xserver.enable = true;
   services.xserver.windowManager.i3.enable = true;
   services.xserver.windowManager.i3.configFile = ./config;
+  services.xserver.desktopManager.wallpaper.combineScreens = false;
+  services.xserver.desktopManager.wallpaper.mode = "fill";
 }
